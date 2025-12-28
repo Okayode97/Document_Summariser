8894a093ed2ea35e66f3966f1959d5252f7273fb
Kayode Sonaike <Okayode97@gmail.com>
2025-12-28 17:46:38 +0000

    - Added handlers for different file types (.md, .txt, .py) - redefined main prompt to be a constant - updated args for summarise_document abstract method - defined model for OpenAISummariser

--- document_summariser.py
+++ document_summariser.py
@@ -4,11 +4,12 @@ from openai import OpenAI
 from pathlib import Path
 from abc import ABC, abstractmethod
 
+PROMPT = "Can you provide a clear and concise summary on the contents of the uploaded file."
 SUPPORTED_FILE_TYPES = [".pdf", ".txt", ".word", ".py", ".md"]
 
 class LLMModelBackend(ABC):
     @abstractmethod
-    def summarise_document(self):
+    def summarise_document(self, path: Path, prompt: str):
         pass
 
 class OpenAISummariser(LLMModelBackend):
@@ -18,26 +19,94 @@ class OpenAISummariser(LLMModelBackend):
         if OPENAI_API_KEY is None:
             raise ValueError("OPENAI_API_KEY is not defined in local environment. Set API key to continue.")
         self.client = OpenAI(api_key=OPENAI_API_KEY)
+        self.model = "gpt-4.1-nano"
     
-    def load_pdf_file(self, document_path: Path):
-        pass
+    def summarise_document(self, document_path: Path, prompt: str):        
+        if document_path.suffix == ".pdf":
+            self.handle_pdf_document(document_path, prompt)
+        
+        if document_path.suffix in [".txt", ".py", ".md"]:
+            self.handle_text_document(document_path, prompt)
+
+    def handle_pdf_document(self, document_path: Path, prompt: str):
+
+        # upload the pdf file to openai
+        file = self.client.files.create(file=open(str(document_path), "rb"), purpose="user_data",
+            expires_after={"anchor": "created_at", "seconds": 2592000}) # deletes the file after 30 days
+
+        # get chat to summarise the contents of the model
+        response = self.client.responses.create(model=self.model,
+            input=[
+                {
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "input_file",
+                            "file_id": file.id,
+                        },
+                        {
+                            "type": "input_text",
+                            "text": f"File type: {document_path.suffix}"
+                        },
+                        {
+                            "type": "input_text",
+                            "text": prompt
+                        }
+                    ]
+                }
+            ]
+        )
+        print(f"Generated Response: {response.output_text}")
+
+        # delete the file after it's been uploaded and summarised
+        self.client.files.delete(file.id)
+
+    def handle_text_document(self, document_path: Path, prompt: str):
+
+        # open the file and read it's contents
+        with open(document_path, "r", encoding="utf-8") as f:
+            extracted_text = f.read()
+
+        # get chat to summarise the contents of the model
+        response = self.client.responses.create(model=self.model,
+            input=[
+                {
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "input_file",
+                            "file_id": f"Document contents.\n\n{extracted_text}",
+                        },
+                        {
+                            "type": "input_text",
+                            "text": f"File type: {document_path.suffix}"
+                        },
+                        {
+                            "type": "input_text",
+                            "text": prompt
+                        }
+                    ]
+                }
+            ]
+        )
+
+        print(response.output_text)
+
 
 class LLMDocumentSummariser:
 
     def __init__(self):
         self.model_summariser = OpenAISummariser()
-        pass
+        self.prompt = PROMPT
 
     def summarise_document(self, document_path: Path):
 
         if not (document_path.suffix in SUPPORTED_FILE_TYPES):
             raise ValueError(f"Unsupported document type uploaded.\nSupported file types are {SUPPORTED_FILE_TYPES} and uploaded file `{document_path.name}`")
         
-        # determine file type
-
-
-        # modify how call to summarise model is made based on file type
-
+        # call the summarise document function
+        # - model summariser class would have handlers for different file types
+        self.model_summariser.summarise_document(document_path, self.prompt)
 
 
 if __name__ == "__main__":
