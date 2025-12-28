7f06dcefdd56a96b460de6645afe1711be0f7bde
Kayode Sonaike <Okayode97@gmail.com>
2025-12-28 16:34:35 +0000

    - Updated document_summariser to use argparse to allow document path to   be passed from cli - defined list of supported file type - defined main class to wrap around different model backend

--- document_summariser.py
+++ document_summariser.py
@@ -1,41 +1,50 @@
-import os
 import argparse
+import os
 from openai import OpenAI
+from pathlib import Path
+from abc import ABC, abstractmethod
+
+SUPPORTED_FILE_TYPES = [".pdf", ".txt", ".word", ".py", ".md"]
+
+class LLMModelBackend(ABC):
+    @abstractmethod
+    def summarise_document(self):
+        pass
+
+class OpenAISummariser(LLMModelBackend):
+    
+    def __init__(self):
+        OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+        if OPENAI_API_KEY is None:
+            raise ValueError("OPENAI_API_KEY is not defined in local environment. Set API key to continue.")
+        self.client = OpenAI(api_key=OPENAI_API_KEY)
+    
+    def load_pdf_file(self, document_path: Path):
+        pass
+
+class LLMDocumentSummariser:
+
+    def __init__(self):
+        self.model_summariser = OpenAISummariser()
+        pass
+
+    def summarise_document(self, document_path: Path):
+
+        if not (document_path.suffix in SUPPORTED_FILE_TYPES):
+            raise ValueError(f"Unsupported document type uploaded.\nSupported file types are {SUPPORTED_FILE_TYPES} and uploaded file `{document_path.name}`")
+        
+        # determine file type
 
 
-# get openai api key from enviroment variables
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-client = OpenAI(api_key=OPENAI_API_KEY)
+        # modify how call to summarise model is made based on file type
 
-# following steps from OpenAI quickstart
-# load the document
-file = client.files.create(
-    file=open(r".\test_documents\frank-a5.pdf", "rb"),
-    purpose="user_data",
-    expires_after={
-        "anchor": "created_at",
-        "seconds": 2592000 # deletes the file after 30 days
-    }
-)
 
-response = client.responses.create(
-    model="gpt-4.1-nano",
-    input=[
-        {
-            "role": "user",
-            "content": [
-                {
-                    "type": "input_file",
-                    "file_id": file.id,
-                },
-                {
-                    "type": "input_text",
-                    "text": "Can you provide a clear and concise summary on the contents of the uploaded file."
-                }
-            ]
-        }
-    ]
-)
 
-# delete the file after it's been uploaded and summarised
-client.files.delete(file.id)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Summarise contents of loaded document")
+    parser.add_argument('doc_path', type=str,
+                        help=f"Path to document, to be uploaded and summarised. Currently supported documents are {SUPPORTED_FILE_TYPES}")
+
+    args = parser.parse_args()
+    llm_doc_summariser = LLMDocumentSummariser()
+    llm_doc_summariser.summarise_document(Path(args.doc_path))
--- log_notes.md
+++ log_notes.md
@@ -8,3 +8,7 @@
 
 from notes on file input the model can accept PDF files as input.
 The PDF files are provided as either Base64-encoded data or as file IDs obtained after uploading the files to the v1/files endpoint via the API or dashboard. In terms of extracting data and knowledge from the PDF content. Images of each page and the extracted text are used by the model to generate a response. w.r.t to using file input, only models that support text & image inputs can accept PDF files as input. This is currently limited to just gpt-4o, gpt-4o-mini, o1. W.r.t to file size. the size of individual files can be up to 512MB and the total size of the file uploaded by one account can be up to 1TB. looking through the API doc it seems that OI stores all the individual files that are uploaded during each test (Only files with purpose=batch are deleted after 30 days for others they persist until they are deleted.) Further update, you can also delete the uploaded file via the API endpoint.
+
+while looking through how i can upload other types of document to the LLM model and get a summary of it, i've found a [platform](https://community.openai.com/t/chatpdf-com-chat-with-any-pdf-using-the-new-chatgpt-api/81446) that offers the capabilities i'm interested in.
+
+Additional notes
